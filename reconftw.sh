c413b3cffdfeb5f097767d17b2a5aa1ede7e6cd2
0xabsiddique <68904184+0xabsiddique@users.noreply.github.com>
2022-08-09 23:21:57 +0600

    Update reconftw.sh

--- reconftw.sh
+++ reconftw.sh
@@ -643,7 +643,7 @@ function screenshot(){
 		if [ ! "$AXIOM" = true ]; then
 			#[ -s ".tmp/webs_all.txt" ] && webscreenshot -i .tmp/webs_all.txt -w $WEBSCREENSHOT_THREADS -o screenshots 2>>"$LOGFILE" &>/dev/null
 			#[ -s ".tmp/webs_all.txt" ] && gowitness file -f .tmp/webs_all.txt -t $GOWITNESS_THREADS --disable-logging 2>>"$LOGFILE"
-			[ -s ".tmp/webs_all.txt" ] && cat.tmp/webs_all.txt | aquatone -http-timeout 10000 -scan-timeout 300 -out screenshotss -threads $auquatoneThreads -silent 2>>"$LOGFILE"
+			[ -s ".tmp/webs_all.txt" ] && cat tmp/webs_all.txt | aquatone -http-timeout 10000 -scan-timeout 300 -out screenshotss -threads $auquatoneThreads -silent 2>>"$LOGFILE"
 		else
 			[ "$AXIOM_SCREENSHOT_MODULE" = "webscreenshot" ] && axiom-scan .tmp/webs_all.txt -m $AXIOM_SCREENSHOT_MODULE -w $WEBSCREENSHOT_THREADS -o screenshots $AXIOM_EXTRA_ARGS 2>>"$LOGFILE" &>/dev/null
 			[ "$AXIOM_SCREENSHOT_MODULE" != "webscreenshot" ] && axiom-scan .tmp/webs_all.txt -m $AXIOM_SCREENSHOT_MODULE -o screenshots $AXIOM_EXTRA_ARGS 2>>"$LOGFILE" &>/dev/null
