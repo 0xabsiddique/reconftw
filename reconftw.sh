378c51810f84bdbf282350acfcbfe47ffeeebeb8
0xabsiddique <68904184+0xabsiddique@users.noreply.github.com>
2022-08-15 14:20:51 +0600

    Update reconftw.sh

--- reconftw.sh
+++ reconftw.sh
@@ -1825,7 +1825,9 @@ if [ -n "$inScope_file" ]; then
         printf "\n\n${bred} In Scope file is not a text file${reset}\n\n"
         exit
     else
-    	mkdir -p $domain .tmp .log .called_fn subdomains webs vulns
+        SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+        dir="$SCRIPTPATH/Recon/$domain"
+    	mkdir -p $dir/$domain $dir/.tmp $dir/.log $dir/.called_fn $dir/subdomains $dir/webs $dir/vulns
     	[ -s "${inScope_file}" ] && cat ${inScope_file} | anew -q subdomains/subdomains.txt
 	cat subdomains/subdomains.txt | httpx ${HTTPX_FLAGS} -no-color -json -H "${HEADER}" -threads $HTTPX_THREADS -rl $HTTPX_RATELIMIT -retries 2 -timeout $HTTPX_TIMEOUT -o .tmp/web_full_info_probe.txt 2>>"$LOGFILE" &>/dev/null
 	cat .tmp/web_full_info_probe.txt 2>/dev/null | anew -q .tmp/webs_all.txt
