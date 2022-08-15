d929419ee4bfbb1890a707edd1e2fa4ead2a2324
0xabsiddique <68904184+0xabsiddique@users.noreply.github.com>
2022-08-15 17:25:49 +0600

    Update reconftw.sh

--- reconftw.sh
+++ reconftw.sh
@@ -1825,13 +1825,8 @@ if [ -n "$inScope_file" ]; then
         printf "\n\n${bred} In Scope file is not a text file${reset}\n\n"
         exit
     else
-        SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-        dir="$SCRIPTPATH/Recon/"
-	cd "$dir"
-	mkdir -p "$domain"
-	cd "$domain"
     	mkdir -p .tmp .log .called_fn subdomains webs vulns fuzzing
-    	[ -s "${inScope_file}" ] && cat ${inScope_file} | anew -q subdomains/subdomains.txt
+    	[ -s "${inScope_file}" ] && cat ${inScope_file} | anew -q $dir/subdomains/subdomains.txt
 	cat subdomains/subdomains.txt | httpx ${HTTPX_FLAGS} -no-color -json -H "${HEADER}" -threads $HTTPX_THREADS -rl $HTTPX_RATELIMIT -retries 2 -timeout $HTTPX_TIMEOUT -o .tmp/web_full_info_probe.txt 2>>"$LOGFILE" &>/dev/null
 	cat .tmp/web_full_info_probe.txt 2>/dev/null | anew -q .tmp/webs_all.txt
 
